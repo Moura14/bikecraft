9ece1b96c756ad33f8a0a92a7f8276fe5e478d63
João Gabriel <joaogabrielmoura14@gmail.com>
2022-07-07 14:56:14 -0300

    inserção do JavaScript - parte 2

--- js/script.js
+++ js/script.js
@@ -45,4 +45,28 @@ function eventosPerguntas(pergunta) {
 
 perguntas.forEach(eventosPerguntas);
 
-console.log(perguntas);
+// Galeria de bicicletas
+
+const galeira = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 100px)").matches;
+
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeira.forEach(eventosGaleria);
+
+//Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
